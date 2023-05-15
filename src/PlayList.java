0875c38ee757097940c694e0f0f09b18100ee5b1
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-05-16 01:24:13 +0530

    PlayList Change  Added

--- src/PlayList.java
+++ src/PlayList.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class PlayList {
 
@@ -9,7 +10,7 @@ public class PlayList {
 
     public PlayList(String title) {
         this.title = title;
-        this.songs=new ArrayList<>();
+        this.songs = new ArrayList<>();
     }
 
     public String getTitle() {
@@ -19,16 +20,28 @@ public class PlayList {
     public void setTitle(String title) {
         this.title = title;
     }
+
     public List<Song> getSongs() {
         return songs;
     }
 
-//    add song from album to PlayList
-    public String addSongFromAlbum(Album album,String songName){
-     Boolean songExists=album.findSong(songName);
-     Song song;
-     if(songExists){
-         song=album.searchSong(songName);
-     }
-     }
+    //    add song from album to PlayList
+    public String addSongFromAlbum(Album album,String songName) {
+//     Boolean songExists=album.findSong(songName);  //o(N)
+//     Song song;
+//     if(songExists){
+//         song=album.searchSong(songName);  //o(N)
+//     }
+//     }
+        //both information found together //True/False and Song using Optional;
+        Optional<Song> songOpt=album.searchSong(songName);
+        if(songOpt.isEmpty()) return "Song not found in album"; //song not found
+        Song songInAlbum=songOpt.get();  //return a song
+        this.songs.add(songInAlbum);
+        return "Song added Successfully";
+    }
+
+
+
+
 }
