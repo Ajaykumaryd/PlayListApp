8513cbcad440bb06719fccf69c1cbf8eb69c4252
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-05-16 03:36:08 +0530

    changes to next and prevoius done

--- src/PlayList.java
+++ src/PlayList.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Optional;
 
 public class PlayList {
@@ -8,9 +9,13 @@ public class PlayList {
 
     private List<Song> songs;
 
+    private boolean wasNext=false;
+
+    private ListIterator<Song> itr;
     public PlayList(String title) {
         this.title = title;
         this.songs = new ArrayList<>();
+        itr=songs.listIterator();
     }
 
     public String getTitle() {
@@ -39,12 +44,46 @@ public class PlayList {
         if(songOpt.isEmpty()) return "Song not found in album"; //song not found
         Song songInAlbum=songOpt.get();  //return a song
         this.songs.add(songInAlbum);
+        itr=songs.listIterator(); //updates the list iterator
         return "Song added Successfully";
     }
 
+//    wasNext=true; it means itr is on the next
+//    wasNext=false;it means itr is on the previous
+     public  String playNext(){
+      if(wasNext){ //it means we are one current song
 
+      }
 
+      if(!wasNext){    //itr is on the previous
+         wasNext=true;
+         itr.next();  //we move the song to current one
+      }
 
+      if(itr.hasNext()){
+          wasNext=true;
+          Song song=itr.next();  //to play the next Song
+          return "Next Song is:"+song;
+      }
+      return "You have reached the end of playList";
+     }
 
+    public  String playPrevious(){
+        if(!wasNext){ //it means we are one previous
+
+        }
+
+        if(wasNext){    //itr is on the previous
+            wasNext=false;
+            itr.previous();  //we move the song to current one
+        }
+
+        if(itr.hasPrevious()){
+            wasNext=false;
+            Song song=itr.previous();  //to play the next Song
+            return "Prevois Song is:"+song;
+        }
+        return "You have reached the Start of playList";
+    }
 
 }
